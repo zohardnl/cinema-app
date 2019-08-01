8947bffc762e6c7dea01dfe3765f853eeee3152a
daniel zohar <daniel zohar@herolo.co.il>
2019-08-02 00:02:18 +0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {FavoriteComponent} from "./favorite/favorite.component";
 
-const routes: Routes = [
-  {path: "", component: MovieListComponent},
-  {path: "favorites", component: FavoriteComponent},
-  {path: "**", redirectTo: ""}
-];
+const routes: Routes = [{path: "", component: MovieListComponent}, {path: "favorites", component: FavoriteComponent}, {path: "**", redirectTo: ""}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
