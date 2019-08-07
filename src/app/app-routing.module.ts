c340c690345a7e70e0243faea7f7158210835203
daniel <daniel2588@gmail.com>
2019-08-07 23:41:16 +0200

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import {MovieListComponent} from "./movie-list/movie-list.component";
-import {NgModule} from "@angular/core";
-import {Routes, RouterModule} from "@angular/router";
-import {FavoriteComponent} from "./favorite/favorite.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FavoriteComponent } from "./favorite/favorite.component";
 
-const routes: Routes = [{path: "", component: MovieListComponent}, {path: "favorites", component: FavoriteComponent}, {path: "**", redirectTo: ""}];
+const routes: Routes = [{ path: "", component: MovieListComponent }, { path: "favorites", component: FavoriteComponent }, { path: "**", redirectTo: "" }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
