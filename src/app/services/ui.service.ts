5b6b9937f7c61a610e93ebf407c28b56a736ed08
daniel zohar <daniel zohar@herolo.co.il>
2019-08-11 23:58:58 +0300

    up

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from "@angular/core";
 
 @Injectable({
   providedIn: "root"
@@ -7,13 +7,13 @@ export class UiService {
   searchFlag: boolean;
   favFlag: boolean;
 
-  constructor() { }
+  constructor() {}
 
   scroll(id: number) {
     if (!this.searchFlag && !this.favFlag) {
       setTimeout(() => {
-        document.querySelector(`#drop${id}`).scrollIntoView({ behavior: "smooth" });
-      }, 300);
+        document.querySelector(`#drop${id}`).scrollIntoView({behavior: "smooth"});
+      }, 100);
     } else {
       document.querySelector(".main-movies").scrollTo({
         top: 0,
