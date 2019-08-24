f6f10202e90d34ff432b0b4807142f584883d589
daniel zohar <daniel zohar@herolo.co.il>
2019-08-24 23:09:52 +0300

    up

--- src/app/modal/add-movie/add-movie.component.ts
+++ src/app/modal/add-movie/add-movie.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm, FormGroup, Validators, FormControl } from "@angular/forms";
-import { Movie } from "src/app/models/Movie";
-import { ApiService } from "./../../services/api.service";
-import { MovieServiceService } from "src/app/services/movie-service.service";
-import { environment } from "./../../../environments/environment";
-import { ModalService } from "src/app/services/modal.service";
-import { trimValue } from 'src/app/validators/trim.validator';
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, Validators, FormControl} from "@angular/forms";
+import {Movie} from "src/app/models/Movie";
+import {ApiService} from "./../../services/api.service";
+import {MovieServiceService} from "src/app/services/movie-service.service";
+import {environment} from "./../../../environments/environment";
+import {ModalService} from "src/app/services/modal.service";
+import {trimValue} from "src/app/validators/trim.validator";
 
 @Component({
   selector: "app-add-movie",
   templateUrl: "./add-movie.component.html",
   styleUrls: ["./add-movie.component.scss"]
 })
-export class AddMovieComponent implements OnInit {
+export class AddMovieComponent implements OnInit{
   infoForm: FormGroup;
+  text:string="";
 
-  constructor(private api: ApiService, private movie: MovieServiceService, private modal: ModalService) { }
+  constructor(private api: ApiService, private movie: MovieServiceService, private modal: ModalService) {}
 
   ngOnInit() {
     this.infoForm = new FormGroup({
-      'title': new FormControl(null, [Validators.required, trimValue]),
-      'overView': new FormControl(null, [Validators.required, trimValue]),
-      'releaseDate': new FormControl(null, [Validators.required, trimValue])
+      'title': new FormControl("", [Validators.required,trimValue]),
+      'overView': new FormControl("", [Validators.required,trimValue]),
+      'releaseDate': new FormControl("", [Validators.required,trimValue])
     });
   }
 
