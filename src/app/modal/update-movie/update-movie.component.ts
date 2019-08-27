dfa9e34f5ef4d50d7dd5db229fcf0f90e6b61d02
daniel <daniel2588@gmail.com>
2019-08-27 22:42:45 +0200

    up

--- src/app/modal/update-movie/update-movie.component.ts
+++ src/app/modal/update-movie/update-movie.component.ts
@@ -1,37 +1,41 @@
-import { ModalService } from "src/app/services/modal.service";
-import { Component, OnInit, Inject } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
-import { Movie } from "src/app/models/Movie";
-import { Validators } from "@angular/forms";
-import { MovieServiceService } from "src/app/services/movie-service.service";
-import { trimValue } from "src/app/validators/trim.validator";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ModalService } from 'src/app/services/modal.service';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Movie } from 'src/app/models/Movie';
+import { Validators } from '@angular/forms';
+import { MovieServiceService } from 'src/app/services/movie-service.service';
+import { trimValue } from 'src/app/validators/trim.validator';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
-  selector: "app-update-movie",
-  templateUrl: "./update-movie.component.html",
-  styleUrls: ["./update-movie.component.scss"]
+	selector: 'app-update-movie',
+	templateUrl: './update-movie.component.html',
+	styleUrls: ['./update-movie.component.scss']
 })
 export class UpdateMovieComponent implements OnInit {
-  updateForm: FormGroup;
-  updatedMovie: Movie;
+	updateForm: FormGroup;
+	updatedMovie: Movie;
 
-  constructor(private movieService: MovieServiceService, @Inject(MAT_DIALOG_DATA) public data: any, private modal: ModalService) {}
+	constructor(
+		private movieService: MovieServiceService,
+		@Inject(MAT_DIALOG_DATA) public data: any,
+		private modal: ModalService
+	) {}
 
-  ngOnInit() {
-    this.updatedMovie = this.data.movie;
-    this.updateForm = new FormGroup({
-      title: new FormControl(this.updatedMovie.title.trim(), [Validators.required, trimValue]),
-      overView: new FormControl(this.updatedMovie.overview.trim(), [Validators.required, trimValue]),
-      releaseDate: new FormControl(this.updatedMovie.release_date.trim(), [Validators.required, trimValue])
-    });
-  }
+	ngOnInit() {
+		this.updatedMovie = this.data.movie;
+		this.updateForm = new FormGroup({
+			title: new FormControl(this.updatedMovie.title.trim(), [Validators.required, trimValue]),
+			overView: new FormControl(this.updatedMovie.overview.trim(), [Validators.required, trimValue]),
+			releaseDate: new FormControl(this.updatedMovie.release_date.trim(), [Validators.required, trimValue])
+		});
+	}
 
-  onUpdateMovie() {
-    let newMovie = new Movie();
-    newMovie.title = this.updateForm.value.title;
-    newMovie.overview = this.updateForm.value.overView;
-    newMovie.release_date = this.updateForm.value.releaseDate;
-    this.movieService.updateMovie(this.updatedMovie, newMovie);
-    this.modal.closeDialog();
-  }
+	onUpdateMovie() {
+		let newMovie = new Movie();
+		newMovie.title = this.updateForm.value.title;
+		newMovie.overview = this.updateForm.value.overView;
+		newMovie.release_date = this.updateForm.value.releaseDate;
+		this.movieService.updateMovie(this.updatedMovie, newMovie);
+		this.modal.closeDialog();
+	}
 }
